a3eafa334b31261b162b27ebea96bc15e36ead84
Nelkisa Matias <nelkisa.matias@nelkisa.matias>
2022-05-09 19:39:38 -0300

    função nova-compra

--- src/yes_she_codes/core.clj
+++ src/yes_she_codes/core.clj
@@ -3,17 +3,27 @@
 (defn novo-cliente
   "retorna um novo cliente com os dados informados"
   [nome cpf email]
-  (let [cliente {:nome nome
+  (let [novo-cliente {:nome nome
                  :cpf cpf
                  :email email}]
-    cliente))
+    novo-cliente))
 
 (defn novo-cartao
   "retorna um cartão de crédito"
   [numero cvv validade limite cpf-cliente]
-  (let [cartao {:numero numero
+  (let [novo-cartao {:numero numero
                 :cvv cvv
                 :validade validade
                 :limite limite
                 :cliente cpf-cliente}]
-    cartao))
+    novo-cartao))
+
+(defn nova-compra
+  "retorna uma nova compra"
+  [data valor estabelecimento categoria numero-cartao]
+  (let [nova-compra {:data data
+                :valor valor
+                :estabelecimento estabelecimento
+                :categoria categoria
+                :cartao numero-cartao}]
+    nova-compra))
