1f062c79e44ac85582a0d8b7fafcf22604a330e2
Nelkisa Matias <nelkisa.matias@nelkisa.matias>
2022-05-10 20:50:30 -0300

    listagem (clientes, cartões e compras) e total-gasto

--- src/yes_she_codes/core.clj
+++ src/yes_she_codes/core.clj
@@ -1,22 +1,44 @@
 (ns yes-she-codes.core)
 
+(def clientes [])
+
 (defn novo-cliente
   "retorna um novo cliente com os dados informados"
   [nome cpf email]
   (let [novo-cliente {:nome nome
-                 :cpf cpf
-                 :email email}]
+                      :cpf cpf
+                      :email email}]
+    (def clientes (conj clientes novo-cliente))
     novo-cliente))
+
+(defn lista-clientes
+  ([arg1] clientes)
+  ([] clientes))
+
+
+
+(def cartoes [])
 
 (defn novo-cartao
   "retorna um cartão de crédito"
   [numero cvv validade limite cpf-cliente]
   (let [novo-cartao {:numero numero
-                :cvv cvv
-                :validade validade
-                :limite limite
-                :cliente cpf-cliente}]
+                     :cvv cvv
+                     :validade validade
+                     :limite limite
+                     :cliente cpf-cliente}]
+    (def cartoes (conj cartoes novo-cartao))
     novo-cartao))
+
+(defn lista-cartoes
+  ([arg1] cartoes)
+  ([] cartoes))
+
+
+
+(def compras [{:data "2022-01-01", :valor 150, :estabelecimento "Outback", :categoria "Alimentação", :cartao 1234123412341234}
+              {:data "2022-01-01", :valor 150, :estabelecimento "Outback", :categoria "Alimentação", :cartao 1234123412341235}
+              {:data "2022-01-01", :valor 150, :estabelecimento "Outback", :categoria "Alimentação", :cartao 1234123412341236}])
 
 (defn nova-compra
   "retorna uma nova compra"
@@ -26,4 +48,30 @@
                 :estabelecimento estabelecimento
                 :categoria categoria
                 :cartao numero-cartao}]
+    (def compras (conj compras nova-compra))
     nova-compra))
+
+(defn teste
+  ([arg1] arg1)
+  ([arg1 arg2] "socorro deus")
+  ([] "aaaaaaaaaa")
+  )
+
+(defn lista-compras
+  ([arg1] compras)
+  ([] compras))
+
+(defn total-gasto-errado [compras]
+  (let [soma 0]
+    (+ soma (:valor compras))))
+
+(defn total-gasto-anonima [compras]
+  (reduce #(+ %1 (:valor %2)) 0 compras))
+
+(defn total-gasto [compras]
+  (reduce (fn [soma compra]
+            (+ soma (:valor compra)))
+          0 compras))
+
+;(println (map total-gasto (lista-compras)))
+;(println (total-gasto (lista-compras)))
