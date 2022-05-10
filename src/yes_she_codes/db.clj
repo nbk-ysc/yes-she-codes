f77eda452ba658ece02f319ae65cb61b57ae5b3a
ana.ssilva <anacss2198@gmail.com>
2022-05-10 17:47:34 -0300

    testando commit

--- src/yes_she_codes/db.clj
+++ src/yes_she_codes/db.clj
@@ -1,3 +1,24 @@
 (ns yes-she-codes.db)
 
 (println "testando")
+
+(def novo-cliente {:nome "Feiticeira Escarlate"
+                     :cpf "000.111.222-33"
+                     :email "feiticeira.poderosa@vingadoras.com.br"})
+;
+;(def novo-cartao {:numero 1234 1234 1234 1234
+;
+;                  :validade (Long)
+;                  :limite (BigDecimal)
+;                  :cliente (String)})
+;
+;(def nova-compra {:data (String: yyyy-mm-dd)
+;                  :valor (BigDecimal)
+;                  :estabelecimento (String)
+;                  :categoria (String)
+;                  :cartão (Long)})
+
+;novo-cartao, nova-compra
+
+(defn banco-de-dados []
+  [novo-cliente])
--- src/yes_she_codes/logic.clj
+++ src/yes_she_codes/logic.clj
@@ -1,3 +1,6 @@
-(ns yes-she-codes.logic)
+(ns yes-she-codes.logic
+  (:require [yes-she-codes.db :as y.db]))
+
+(println (y.db/banco-de-dados))
 
 (println "teste do REPL logic")
