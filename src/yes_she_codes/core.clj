e798f237e9c3d199c6d0c0fd682e61ddf08d6c62
emimuniz <emillyn_muniz@hotmail.com>
2022-05-10 12:21:57 -0300

    Refactor novo-client

--- src/yes_she_codes/core.clj
+++ src/yes_she_codes/core.clj
@@ -1,13 +1,17 @@
 (ns yes-she-codes.core)
 
-(defn novo-client
-  [nome cpf email]
+(defn novo-cliente
+  [[nome cpf email]]
   (let [client (conj {:nome nome :cpf cpf :email email})]
-    [client] ))
+    client))
 
-(println (novo-client "Emillyn" "8239283298" "fulano@hotmail.com"))
+(defn lista-clients []
+  (let [clients [["feiticeira escarlate" "000.111.222-33" "feiticeira.poderosa@vingadoras.com.br"]
+                 ["Viúva Negra" "333.444.555-66" "viuva.casca.grossa@vingadoras.com.br"]
+                 ["Hermione Granger" "666.777.888-99" "hermione.salvadora@hogwarts.com"]
+                 ["Daenerys Targaryen" "999.123.456-78" "mae.dos.dragoes@got.com"]]]
+               (vec (map novo-cliente clients))))
 
 
-
-
+(println (lista-clients))
 
