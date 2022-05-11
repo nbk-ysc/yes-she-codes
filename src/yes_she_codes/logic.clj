b1cedad4f133ab729471cf8c900fc3104105eb1c
isabella.barbosa <bellargbarbosa@hotmail.com>
2022-05-11 10:53:35 -0300

    Novas funcoes: valores dentro de um determinado intervalo de valor e agrupamento de compras por estabelecimento

--- src/yes_she_codes/logic.clj
+++ src/yes_she_codes/logic.clj
@@ -28,3 +28,30 @@
        (buscar-compras-mes mes)
        total-gasto))
 
+(defn valor-dentro-do-intervalo?
+  [valor-minimo valor-maximo compra]
+  (if (< valor-maximo valor-minimo)
+    (let [valor-maximo-atualizado valor-minimo
+          valor-minimo-atualizado valor-maximo]
+      (and (> (:valor compra) valor-minimo-atualizado) (< (:valor compra) valor-maximo-atualizado)))
+
+    (and (> (:valor compra) valor-minimo) (< (:valor compra) valor-maximo)))
+  )
+
+(defn compras-por-intervalo-de-valor
+  [valor-minimo valor-maximo compras]
+  (filter #(valor-dentro-do-intervalo? valor-minimo valor-maximo %) compras))
+
+(defn total-gasto-por-categoria
+  [[nome estabelecimento]]
+  {:estabelecimento nome
+   :total           (->> estabelecimento
+                         (map :valor)
+                         (reduce +))})
+
+(defn gastos-agrupados-por-categoria
+  [compras]
+  (->> compras
+       (group-by :estabelecimento)
+       (map total-gasto-por-categoria)))
+
