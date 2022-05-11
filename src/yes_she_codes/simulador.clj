313fac35ba8d75e887bd3c4edcbb014774cccbbd
giovannabbottino <giovannabbottino@gmail.com>
2022-05-11 15:51:26 -0300

    Calcular o total da fatura de um mês

--- src/yes_she_codes/simulador.clj
+++ src/yes_she_codes/simulador.clj
@@ -4,8 +4,15 @@
 (defn total-gasto [lista-compras]
   (apply + (map :valor lista-compras)))
 
-(defn busca-compras-mes [lista-compras mes]
+(defn busca-compras-mes [mes lista-compras]
   (filter #(str/includes? (:data %) mes) lista-compras))
 
-(defn busca-compras-estabelecimento [lista-compras estabelecimento]
+(defn busca-compras-estabelecimento [estabelecimento lista-compras]
   (filter #(= (str/lower-case (:estabelecimento %)) (str/lower-case estabelecimento)) lista-compras))
+
+(defn total-gasto-no-mes [cartao mes lista-compras]
+  (->> lista-compras
+      (filter #(= (:cartao %) cartao) ,,,)
+      (busca-compras-mes mes ,,,)
+      (total-gasto ,,,)
+      ))
--- test/yes_she_codes/simulador_test.clj
+++ test/yes_she_codes/simulador_test.clj
@@ -10,9 +10,9 @@
 
 (deftest busca-compras-mes-test
   (testing "Testando buscar compras por mes"
-    (is (= (busca-compras-mes [{:valor 100.5 :data "2022-01-01"}
+    (is (= (busca-compras-mes "2022-01" [{:valor 100.5 :data "2022-01-01"}
                                {:valor 250 :data "2022-03-02"}
-                               {:valor 400 :data "2022-01-02"}] "2022-01")
+                               {:valor 400 :data "2022-01-02"}])
            [{:data  "2022-01-01"
              :valor 100.5}
             {:data  "2022-01-02"
@@ -20,12 +20,19 @@
 
 (deftest busca-compras-estabelecimento-test
   (testing "Testando buscar compras por estabelecimento"
-    (is (= (busca-compras-estabelecimento [{:valor 100.5 :data "2022-01-01" :estabelecimento "praia"}
+    (is (= (busca-compras-estabelecimento  "praia" [{:valor 100.5 :data "2022-01-01" :estabelecimento "praia"}
                                {:valor 250 :data "2022-03-02" :estabelecimento "Oficina"}
-                               {:valor 400 :data "2022-01-02" :estabelecimento "Praia"}] "praia")
+                               {:valor 400 :data "2022-01-02" :estabelecimento "Praia"}])
            [{:data  "2022-01-01"
              :valor 100.5
              :estabelecimento "praia"}
             {:data  "2022-01-02"
              :valor 400
              :estabelecimento "Praia"}] ))))
+
+(deftest total-gasto-no-mes-test
+  (testing "Testando total gasto no mes"
+    (is (= (total-gasto-no-mes 1234123412341234 "2022-01" [{:valor 100.5 :data "2022-01-01" :estabelecimento "praia" :cartao 1234123412341234}
+                                                           {:valor 250 :data "2022-03-02" :estabelecimento "Oficina" :cartao 4321432143214321}
+                                                           {:valor 400 :data "2022-01-02" :estabelecimento "Praia" :cartao 1234123412341234}])
+           500.5 ))))
