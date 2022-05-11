71f5133ada91ea9083b00c56e697f7a0221ec3b1
giovannabbottino <giovannabbottino@gmail.com>
2022-05-11 11:55:35 -0300

    Buscar compras por mês

--- src/yes_she_codes/simulador.clj
+++ src/yes_she_codes/simulador.clj
@@ -1,5 +1,8 @@
-(ns yes-she-codes.simulador)
-
+(ns yes-she-codes.simulador
+  (:require [clojure.string :as str]))
 
 (defn total-gasto [lista-compras]
   (apply + (map :valor lista-compras)))
+
+(defn busca-compras-mes [lista-compras mes]
+  (filter #(str/includes? (:data %) mes ) lista-compras))
--- test/yes_she_codes/simulador_test.clj
+++ test/yes_she_codes/simulador_test.clj
@@ -7,3 +7,11 @@
     (is (= (total-gasto [{:valor 100.5}
                          {:valor 250}
                          {:valor 400}]) 750.50))))
+
+(deftest busca-compras-mes-test
+  (testing "Testando buscar compras por mes"
+    (is (= (busca-compras-mes [{:data "2022-01-01"}
+                               {:data "2022-03-02"}
+                               {:data "2022-01-02"}] "2022-01")
+           [{:data "2022-01-01"}
+            {:data "2022-01-02"}] ))))
