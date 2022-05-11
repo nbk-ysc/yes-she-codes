81d48db69d12f70dfc8153d330f4b1888089c909
emimuniz <emillyn_muniz@hotmail.com>
2022-05-11 12:27:57 -0300

    criacao da funcao nova-compra

--- src/yes_she_codes/core.clj
+++ src/yes_she_codes/core.clj
@@ -15,12 +15,11 @@
                  ["Daenerys Targaryen" "999.123.456-78" "mae.dos.dragoes@got.com"]]]
                (swap! conta-clients assoc-in [:clients] (vec (map novo-cliente clients)))))
 
-
 (lista-clients)
 (println  @conta-clients)
 
 (defn get-client
-  [field value data]
+  [field  value data]
   (filter #(= value (field %)) data))
 
 (defn add-cartao [conta]
@@ -29,10 +28,23 @@
 (defn novo-cartao
   [numero cvv validate limite cpf]
   (let [client (into {} (get-client :cpf cpf (:clients @conta-clients)))]
-  (let [cartao {:conta {:numero numero, :cvv cvv, :validate validate :limite limite}}]
+  (let [cartao {:cartao {:numero numero, :cvv cvv, :validate validate :limite limite}}]
   (let [conta (conj client cartao)]
     (add-cartao conta)))))
+
+(defn nova-compra
+  [data valor estabelecimento categoria cartao]
+  (let [client (into {} (get-client #(:numero (:cartao %)) cartao (:clients @conta-clients)))]
+  (let [compra {:compras {:data data, :valor valor, :estabelecimento estabelecimento :categoria categoria}}]
+  (let [conta (assoc-in client [:cartao] (conj (:cartao client) compra))]
+    conta))))
 
 (println (get-client :cpf "000.111.222-33" (:clients @conta-clients)))
 
 (println (novo-cartao 12819289182918 233 "2022/12" 1000.0 "000.111.222-33"))
+
+(println (get-client (comp :numero :cartao) 12819289182918 (:clients @conta-clients)))
+(println (get-client #(:numero (:cartao %)) 12819289182918 (:clients @conta-clients)))
+
+(println (nova-compra "01/2012" 1000 "Americanas" "Alimentacao" 12819289182918))
+(println (nova-compra "01/2013" 1000 "Ifood" "Alimentacao" 12819289182918))
