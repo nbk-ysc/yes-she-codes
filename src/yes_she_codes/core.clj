33aba6a2296e9b9e9c20de47bc760a8d44753b76
DaianedaSilva <daiane.supereira@gmail.com>
2022-05-12 14:55:18 -0300

    creatre nova-compraZ git add .

--- src/yes_she_codes/core.clj
+++ src/yes_she_codes/core.clj
@@ -16,8 +16,24 @@
    "CVV" CVV
    "validade" validade
    "limite" limite
-   "cliente" cliente
-   })
+   "cliente" cliente})
 
 (def cliente1 (novo-cliente "daiane" "44219671814" "daiane@gmail.com"))
-(println (novo-cartao 1234568521236 442 "2030-07" 1500.0M cliente1))
+(def cartao1 (novo-cartao 1234568521236 442 "2030-07" 1500.0M cliente1 ))
+
+(println cartao1)
+
+(defn nova-compra
+  [data valor estabelecimento categoria cartao]
+  {"data" data
+   "valor" valor
+   "estabelecimento" estabelecimento
+   "categoria" categoria
+   "cartao" cartao})
+
+(def compra1 (nova-compra "2022-05-12" 150.0M "palace" "beleza" cartao1))
+(println compra1)
+
+
+
+
