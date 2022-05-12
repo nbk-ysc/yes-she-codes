e19c3a4df891723e8813812192d1a10d1a2a4759
DaianedaSilva <daiane.supereira@gmail.com>
2022-05-12 14:49:51 -0300

    create novo-cartao

--- src/yes_she_codes/core.clj
+++ src/yes_she_codes/core.clj
@@ -1,10 +1,23 @@
-(ns yes-she-codes.core)
+(ns yes-she-codes.core
+  (:import (java.awt.image ConvolveOp)))
 
 (defn novo-cliente
   [nome cpf email]
-  {"Nome" nome
+  {"nome" nome
    "CPF" cpf
-   "Email" email})
+   "email" email})
 
 (println (novo-cliente "daiane" "44219671814" "daiane@gmail.com"))
 
+
+(defn novo-cartao
+  [numero CVV  validade limite cliente]
+  {"numero" numero
+   "CVV" CVV
+   "validade" validade
+   "limite" limite
+   "cliente" cliente
+   })
+
+(def cliente1 (novo-cliente "daiane" "44219671814" "daiane@gmail.com"))
+(println (novo-cartao 1234568521236 442 "2030-07" 1500.0M cliente1))
