e0bb25e1de9a6d4ab511fa0131460710b6ceab88
emimuniz <emillyn_muniz@hotmail.com>
2022-05-12 14:38:07 -0300

    Conclusao dos Desafios Inciantes

--- src/yes_she_codes/core.clj
+++ src/yes_she_codes/core.clj
@@ -45,13 +45,65 @@
 (defn lista-compras []
   (:compras (parametros-compras)))
 
-(println (lista-compras))
 
 (defn total-gasto [compras]
   (reduce + (map #(:valor %) compras)))
 
+(defn filtra-compras
+  [field value data]
+  (filter #(= value (field %)) data))
+
+(defn filtra-compras-data
+  [field value data]
+  (filter #(= (re-matches (re-pattern (str "[0-9]{4}-" value "-[0-9]{2}")) (field %)) (field %)) data))
+
+
+(defn buscar-por-mes
+  [mes compras]
+  (filtra-compras-data :data mes compras))
+
+
+(defn total-gasto-mes
+  [mes cartao compras]
+  (let [client-cartao (filtra-compras :cartao cartao compras)]
+    (let [data (filtra-compras-data :data mes client-cartao)]
+      (total-gasto data))))
+
+
+(defn buscar-por-estabelecimento
+  [estabelecimento compras]
+  (filtra-compras :estabelecimento estabelecimento compras))
+
+(defn filtrar-intervalo-compras
+  [minimo maximo compras]
+  (filter #(and (> (compare (% :data) minimo) 0) (< (compare (% :data) maximo) 0)) compras))
+
+
+(defn total-compras-por-categoria
+  [[categoria values]]
+  {:categoria categoria
+   :R$ (total-gasto values)})
+
+
+(defn agrupar-por-categoria [compras]
+  (map total-compras-por-categoria
+     (group-by :categoria compras)))
+
+
+
+(println (lista-compras))
 
 (println (total-gasto (:compras (parametros-compras))))
+
+(println (buscar-por-mes "01" (:compras (parametros-compras))))
+
+(println "total-gasto-mes" (total-gasto-mes "01" "1234 1234 1234 1234" (:compras (parametros-compras))))
+
+(println (agrupar-por-categoria (:compras (parametros-compras))))
+
+(println (buscar-por-estabelecimento "Alura" (:compras (parametros-compras))))
+
+(println (filtrar-intervalo-compras "2022-01-01" "2022-03-01" (:compras (parametros-compras))))
 
 
 
