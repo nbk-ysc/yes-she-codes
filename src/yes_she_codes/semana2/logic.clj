c9e497744b1936f1022eca15cb15f80acd475062
Marta Lima <marta.lima@nubank.com.br>
2022-05-18 17:24:20 -0300

    all essencial functions from week 2 done

--- src/yes_she_codes/semana2/logic.clj
+++ src/yes_she_codes/semana2/logic.clj
@@ -5,3 +5,49 @@
   (if-not (empty? entidades)
     (+ 1 (apply max (map :id entidades)))
     1))
+
+(defn valida-data
+  [data]
+  (if (re-matches #"\d{4}-\d{2}-\d{2}" data)
+    true
+    false))
+
+(defn valida-valor
+  [valor]
+  (and (= (class valor) java.math.BigDecimal) (> valor 0)))
+
+(defn valida-estabelecimento
+  [estabelecimento]
+  (>= (count estabelecimento) 2))
+
+(defn valida-categoria
+  [categoria]
+  (let [categorias ["Alimentação" "Automóvel" "Casa" "Educação" "Lazer" "Saúde"]]
+    (some #(= categoria %) categorias)))
+
+(defn valida-compra
+  [compra]
+  (if (and (valida-data (:data compra))
+           (valida-valor (:valor compra))
+           (valida-estabelecimento (:estabelecimento compra))
+           (valida-categoria (:categoria compra)))
+    true
+    false))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
