fb7234f67ab9e8502cb1fa47b9c1e079303ee9e1
mariana.moreira <maribrignol.98@gmail.com>
2022-05-19 17:56:28 -0300

    Implementing new functions

--- src/yes_she_codes/core.clj
+++ src/yes_she_codes/core.clj
@@ -78,7 +78,7 @@
   (->> (slurp arquivo)
        str/split-lines
        rest
-       (map  #(str/split % #","))
+       (map #(str/split % #","))
        (mapv funcao-mapeamento)))
 
 (defn lista-clientes []
@@ -93,7 +93,7 @@
 
 (defn lista-compras []
   (processa-csv "dados/compras.csv" (fn [[data valor estabelecimento categoria cartao]]
-                                              (y.compra/nova-compra data valor estabelecimento categoria cartao))))
+                                      (y.compra/nova-compra data valor estabelecimento categoria cartao))))
 
 ;SEMANA 2
 
@@ -108,51 +108,77 @@
                    ^String categoria
                    ^Long cartao])
 
+;(pprint (Compra. nil "2022-01-01" 10M "Alura" "Educação" 4321432143214321))
+;(pprint compra-exemplo)
+
 ;DEFINE UMA COMPRA
-(def compra (Compra. nil "2022-01-01" 200 "Alura" "Educação" 4321432143214321))
+(def compra-exemplo (Compra. nil "2022-01-01" 100 "Alura" "Educação" 4321432143214321))
+(def compra-exemplo2 (Compra. nil "2022-01-01" 300 "Hamburguer" "Alimentação" 4321432143214321))
+
+(def lista-de-compras (lista-compras))
 
 ;FUNÇÃO INSERIR COMPRA
-(defn insere-compra [compra compras]
-  (->> compras
-       count
-       inc
-       (assoc compra :id)
-       (conj compras)))
+(defn insere-compra [lista-de-compras compra]
+  (conj lista-de-compras (assoc compra :id (count lista-de-compras))))
 
-;(pprint (insere-compra compra (lista-compras)))
+;(pprint (insere-compra lista-de-compras compra-exemplo))
+
+;FUNÇÃO LISTAR COMPRA NO ÁTOMO
+(defn lista-compras! [repositorio-de-compras]
+  (pprint @repositorio-de-compras))
+
+;(lista-compras! repositorio-de-compras)
 
 ;STOP
 
 ;FUNÇÃO INSERIR COMPRA NO ÁTOMO
-(defn insere-compra! [compra repositorio-de-compras]
-  (swap! repositorio-de-compras conj compra)
-  (swap! repositorio-de-compras conj compra)
-  (swap! repositorio-de-compras conj compra)
-  (pprint @repositorio-de-compras)
-  ;(lista-compras! repositorio-de-compras)
+
+(defn insere-compra! [lista-de-compras compra]
+  (swap! lista-de-compras insere-compra compra)
   )
 
-(insere-compra! compra repositorio-de-compras)
-
-;FUNÇÃO LISTAR COMPRA NO ÁTOMO
-(defn lista-compras! [repositorio-de-compras]
-  (pprint (deref repositorio-de-compras)))
-
+;(lista-compras! repositorio-de-compras)
+(insere-compra! repositorio-de-compras compra-exemplo)
+(insere-compra! repositorio-de-compras compra-exemplo2)
+(lista-compras! repositorio-de-compras)
 
 ;FUNÇÃO PESQUISAR COMPRA POR ID
 (defn pesquisa-compra-por-id [id compras]
-  (-> :id
-      (group-by compras)
-      (get id)
-      pprint))
+  (let [compra-id (-> :id
+                      (group-by compras)
+                      (get id))]
+    (if compra-id
+      (map->Compra compra-id))))
 
-;(pesquisa-compra-por-id 20 (insere-compra compra (lista-compras)))
+(defn pesquisa-compra-por-id2 [id compras]
+  (map->Compra (filter (fn [compra]
+                         (= id  (:id compra))) compras)))
+
+;(pprint (pesquisa-compra-por-id 19 (insere-compra lista-de-compras compra-exemplo)))
+;(pprint (pesquisa-compra-por-id2 19 (insere-compra lista-de-compras compra)))
 
 ;FUNÇÃO PESQUISAR COMPRA POR ATOMO
 (defn pesquisa-compra-por-id! [id repositorio-de-compras]
-  (pprint (deref repositorio-de-compras))
+  (pesquisa-compra-por-id id @repositorio-de-compras)
   )
 
-;(pesquisa-compra-por-id! 20 repositorio-de-compras)
+;(insere-compra! repositorio-de-compras compra)
+;(lista-compras! repositorio-de-compras)
+;(pprint (pesquisa-compra-por-id! 0 repositorio-de-compras))
+
+
+
+;FUNÇÃO EXCLUIR COMPRA
+(defn exclui-compra [id lista-compras]
+  ;(pprint lista-compras)
+  (remove (pesquisa-compra-por-id2 id lista-compras) lista-compras)
+  )
+
+;(pprint (insere-compra lista-de-compras compra-exemplo))
+;(pprint (exclui-compra 19 (insere-compra lista-de-compras compra-exemplo)))
+;(pprint (pesquisa-compra-por-id 19 (insere-compra lista-de-compras compra-exemplo)))
+
+(defn exclui-compra! [id lista-compras]
+  (swap! lista-compras exclui-compra id))
 
 
