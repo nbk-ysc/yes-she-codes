44cb5622099b2e838e6299b9b66b2c3c58088256
Candy Gonzales <candy.gonzales@nubank.com.br>
2022-05-20 15:59:04 -0300

    pesquisa-compra-por-id!

---
+++ src/yes_she_codes/comprasatom.clj
@@ -0,0 +1,53 @@
+(ns yes-she-codes.comprasatom
+  (:require [yes-she-codes.logic :as y.logic])
+  (:use [clojure.pprint]))
+
+
+
+;1- Definir átomo como banco de dados em memória, vazio
+(def repositorio-de-compras (atom []))
+;(pprint (class @repositorio-de-compras))
+
+;2- Criar Record para Compra
+(defrecord Compras [^Long id, ^String data, ^BigDecimal valor, ^String estabelecimento, ^String categoria, ^Long cartao])
+
+;3- Criar a função insere-compra. Ela vai atribuir um id a uma compra e armazená-la num vetor.
+(defn insere-compra [vetor-compras-cadastradas record-compras]
+  (->> vetor-compras-cadastradas
+       (y.logic/proximo-id)
+       (assoc record-compras :id)
+       (conj vetor-compras-cadastradas)))
+
+;4- Inserir compra no átomo
+(defn insere! [atomo-compras record-compras]
+  (swap! atomo-compras into (insere-compra @atomo-compras record-compras)))
+
+;5- Crie a função lista-compras!, que lista uma lista de compras de um átomo.
+(defn lista-compras! [atomo-compras]
+  (println "atomo" @atomo-compras))
+
+;6- Crie a função pesquisa-compra-por-id, que pesquisa por uma compra de determinado id num vetor.
+(defn pesquisa-compra-por-id [id vetor-compras]
+  (->> vetor-compras
+       (filter #(= (:id %) id))
+       first))
+
+;7- Crie a função pesquisa-compra-por-id!, que pesquisa por uma compra em um átomo.
+(defn pesquisa-compra-por-id! [id atomo-compras]
+  (pesquisa-compra-por-id id @atomo-compras))
+
+;8- Crie a função exclui-compra, que exclui uma compra de determinado id de um vetor.
+(defn exlui-compra [id vetor-compras]
+  (if-not (= nil (pesquisa-compra-por-id id vetor-compras))
+    (->> vetor-compras
+         (remove #(= (:id %) id))
+         vec)
+    vetor-compras
+    ))
+
+;9- Criar a função exclui-compra! para uma compra de um átomo por meio de swap!.
+(defn exlui-compra! [id atomo-compras]
+  (swap! atomo-compras into (exlui-compra id @atomo-compras)))
+
+
+
---
+++ src/yes_she_codes/testesem2.clj
@@ -0,0 +1,43 @@
+(ns yes-she-codes.testesem2
+  (:require [yes-she-codes.comprasatom :refer :all]
+            [yes-she-codes.model :as y.model])
+  (:use [clojure.pprint])
+  )
+
+(def vetor-de-compras (y.model/lista-compras))
+
+(def compra1 (map->Compras {:data            "2022-05-10",
+                            :valor           100.00M,
+                            :estabelecimento "Alura",
+                            :categoria       "Educação",
+                            :cartao          3939393939393939}))
+(def compra2 (map->Compras {:id              15
+                            :data            "2022-03-10",
+                            :valor           50.00M,
+                            :estabelecimento "Drograria",
+                            :categoria       "Saúde",
+                            :cartao          1234123412341234}))
+(def compra3 (map->Compras {:id              6
+                            :data            "2022-05-10",
+                            :valor           300.00M,
+                            :estabelecimento "Cinema",
+                            :categoria       "Lazer",
+                            :cartao          3939393939393939}))
+
+;;;;;;;testes;;;;;;;;;;;
+;teste 3
+(println "Teste3: ") (pprint (insere-compra vetor-de-compras compra1))
+;teste 4
+(reset! repositorio-de-compras  [])
+(pprint (insere! repositorio-de-compras compra2))
+;teste 5
+(lista-compras! repositorio-de-compras)
+;teste 6
+(pprint (pesquisa-compra-por-id 1 [{:id 1 :data "2022-04-10" :valor 150.00M} {:id 7} compra2]))
+;teste 7
+(println "\n Pesquisa")(pprint (pesquisa-compra-por-id! 4  repositorio-de-compras))
+;teste 8
+(println "\n Vetor:") (pprint vetor-de-compras)
+(println "\n Pesquisa para excluir id:") (pprint (exlui-compra 2 vetor-de-compras))
+;teste 9
+(println "\n Exclui compra desde id:") (pprint (exlui-compra! 1 repositorio-de-compras))
