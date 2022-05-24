b51c409216ef4d35d139910e39b2bc0426d32656
Cácio Costa <cacio.cjcs@gmail.com>
2022-05-24 19:52:41 -0300

    finaliza semana 2 e organiza o projeto em pacotes

--- src/yes_she_codes/core.clj
+++ src/yes_she_codes/core.clj
@@ -1,132 +1,92 @@
-(ns yes-she-codes.core)
-
-(defn str->long [valor]
-  (Long/parseLong (clojure.string/replace valor #" " "")))
-
-
-(defn novo-cliente [nome cpf email]
-  {:nome  nome
-   :cpf   cpf
-   :email email})
-
-
-(defn novo-cartao [numero cvv validade limite cliente]
-  {:numero  (str->long numero)
-   :cvv     (str->long cvv)
-   :email   validade
-   :limite  (bigdec limite)
-   :cliente cliente})
-
-
-(defn nova-compra [data valor estabelecimento categoria cartao]
-  {:data            data
-   :valor           (bigdec valor)
-   :estabelecimento estabelecimento
-   :categoria       categoria
-   :cartao          (str->long cartao)})
-
-
-(defn processa-csv [caminho-arquivo funcao-mapeamento]
-  (->> (slurp caminho-arquivo)
-       clojure.string/split-lines
-       rest
-       (map #(clojure.string/split % #","))
-       (mapv funcao-mapeamento)))
-
-
-(defn lista-clientes []
-  (processa-csv "dados/clientes.csv" (fn [[nome cpf email]]
-                                       (novo-cliente nome cpf email))))
-
-(defn lista-cartoes []
-  (processa-csv "dados/cartoes.csv" (fn [[numero cvv validade limite cliente]]
-                                       (novo-cartao numero cvv validade limite cliente))))
-
-
-(defn lista-compras []
-  (processa-csv "dados/compras.csv" (fn [[data valor estabelecimento categoria cartao]]
-            (nova-compra data valor estabelecimento categoria cartao))))
-
-
-(defn total-gasto [compras]
-  (reduce + (map :valor compras)))
-
-
-(defn mes-da-data [data]
-  (second (re-matches #"\d{4}-(\d{2})-\d{2}" data)))
-
-
-(defn filtra-compras [predicado compras]
-  (vec (filter predicado compras)))
-
-
-(defn filtra-compras-no-mes [mes compras]
-  (filtra-compras #(= mes (mes-da-data (:data %)))
-                  compras))
-
-
-(defn filtra-compras-no-estabelecimento [estabelecimento compras]
-  (filtra-compras #(= estabelecimento (:estabelecimento %))
-                  compras))
-
-
-;(defn total-gasto-no-mes [mes compras]
-;  (total-gasto (filtra-compras-no-mes mes compras)))
-
-
-(def total-gasto-no-mes-com-composition (comp total-gasto filtra-compras-no-mes))
-
-(defn filtra-compras-por-valor [minimo maximo compras]
-  (filtra-compras #(and (>= (:valor %) minimo)
-                        (<= (:valor %) maximo))
-                  compras))
-
-
-(defn agrupa-gastos-por-categoria [compras]
-  (vec (map (fn [[categoria compras-da-categoria]]
-              {:categoria   categoria
-               :total-gasto (total-gasto compras-da-categoria)})
-            (group-by :categoria compras))))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+(ns yes-she-codes.core
+  (:require [yes-she-codes.util :as y.util]
+            [yes-she-codes.persistencia.banco-de-dados-atomos :as y.bd]
+            [yes-she-codes.dominio.cliente :as y.cliente]
+            [yes-she-codes.dominio.cartao :as y.cartao]
+            [yes-she-codes.dominio.compra :as y.compra]
+            [java-time :as time])
+  (:use [clojure pprint]))
+
+; Faz carga dos CSVs no banco (átomos)
+(y.bd/carrega-banco-de-dados!)
+
+
+; TESTE DAS LÓGICAS DE CLIENTES
+(println "========= LISTAGEM DE CLIENTES ==========")
+(pprint (y.bd/lista-clientes!))
+
+(println "\n========= INSERE CLIENTE DE ID 5 ==========")
+(y.bd/insere-cliente! (y.cliente/->Cliente nil
+                                           "Mulher Maravilha"
+                                           "321.321.321-32"
+                                           "maisfortedaliga@ligadajustica.dc.br"))
+
+(println "\n========= PESQUISA CLIENTE DE ID 5 ==========")
+(pprint (y.bd/pesquisa-cliente-por-id! 5))
+
+(println "\n========= EXCLUIR CLIENTE DE ID 5 ==========")
+(y.bd/exclui-cliente! 5)
+
+
+; TESTE DAS LÓGICAS DE CARTÕES
+(println "\n========= LISTAGEM DE CARTÕES ==========")
+(pprint (y.bd/lista-cartoes!))
+
+(println "\n========= INSERE CARTÃO DE ID 6 ==========")
+(y.bd/insere-cartao! (y.cartao/->Cartao nil
+                                        1111222233334444
+                                        999
+                                        (time/year-month "2025-05")
+                                        1500M
+                                        "000.111.222-33"))
+
+(println "\n========= PESQUISA CARTÃO DE ID 6 ==========")
+(pprint (y.bd/pesquisa-cartao-por-id! 6))
+
+(println "\n========= EXCLUIR CLIENTE DE ID 5 ==========")
+(y.bd/exclui-cartao! 6)
+
+
+; TESTE DAS LÓGICAS DE COMPRAS
+(println "\n========= LISTAGEM DE COMPRAS ==========")
+(pprint (y.bd/lista-compras!))
+
+(println "\n========= INSERE COMPRA DE ID 20 ==========")
+(y.bd/insere-compra! (y.compra/->Compra nil
+                                        (time/local-date "2022-05-20")
+                                        123.99M
+                                        "Carrefour"
+                                        "Alimentação"
+                                        3939393939393939))
+
+(println "\n========= PESQUISA COMPRA DE ID 20 ==========")
+(pprint (y.bd/pesquisa-compra-por-id! 20))
+
+(println "\n========= EXCLUIR COMPRA DE ID 20 ==========")
+(y.bd/exclui-compra! 20)
+
+(println "\n========= INSPECIONANDO COMPRAS DA Viúva Negra (cartão 4321432143214321)")
+(def compras-da-viuva (y.compra/filtra-compras-por-cartao 4321432143214321
+                                                          (y.bd/lista-compras!)))
+
+(println "\n========= TOTAL GASTO EM TODAS AS COMPRAS =========")
+(pprint (y.compra/total-gasto compras-da-viuva))
+
+(println "\n========= TOTAL GASTO EM FEVEREIRO =========")
+(pprint (y.compra/total-gasto-no-mes 2 compras-da-viuva))
+(pprint (y.compra/filtra-compras-no-mes 2 compras-da-viuva))
+
+(println "\n========= COMPRAS AGRUPADAS POR CATEGORIA =========")
+(pprint (y.compra/agrupa-gastos-por-categoria compras-da-viuva))
+
+
+(println "\n========= TENTA INSERIR COMPRA INVÁLIDA ==========")
+(try
+  (y.bd/insere-compra! (y.compra/->Compra nil
+                                          (time/local-date "2022-05-20")
+                                          -100M
+                                          "Carrefour"
+                                          "Alimentação"
+                                          3939393939393939))
+  (catch Exception e
+    (println "Validação deu certo")))
---
+++ src/yes_she_codes/dominio/cliente.clj
@@ -0,0 +1,5 @@
+(ns yes-she-codes.dominio.cliente)
+
+(defrecord Cliente [id nome cpf email])
+
+
