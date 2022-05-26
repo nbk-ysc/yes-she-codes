19b690e5a66a40a56ce60a5b03b8cace8a74f432
Maria Clara Crespo <70768456+mariaclaracrespo@users.noreply.github.com>
2022-05-26 14:46:42 -0300

    Testes de exception

--- test/yes_she_codes/core_test.clj
+++ test/yes_she_codes/core_test.clj
@@ -3,6 +3,7 @@
             [schema.core :as s]
             [yes-she-codes.week3 :refer :all]))
 
+
 (deftest ClienteSchema-test
   (testing "se o esquema aceita um cliente válido"
     (let [cliente-valido {:nome "Feiticeira Escarlate",
@@ -15,8 +16,26 @@
     (let [cliente-invalido {:nome nil,
                             :cpf "000.111.222-33"
                             :email "feiticeira.poderosa@vingadoras.com.br"}]
+
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (s/validate ClienteSchema cliente-invalido)))))
+
+  (testing "se cliente com :cpf fora do padrão é validado"
+    (let [cliente-invalido {:nome "Feiticeira Escarlate",
+                            :cpf "00011122233"
+                            :email "feiticeira.poderosa@vingadoras.com.br"}]
+
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (s/validate ClienteSchema cliente-invalido)))))
+
+  (testing "se cliente com email fora do padrão é validado"
+    (let [cliente-invalido {:nome "Feiticeira Escarlate",
+                            :cpf "000.111.222-33"
+                            :email "feiticeira.poderosa vingadoras.com.br"}]
+
       (is (thrown? clojure.lang.ExceptionInfo
                    (s/validate ClienteSchema cliente-invalido))))))
+
 
 
 (deftest CartaoSchema-test
@@ -27,7 +46,52 @@
                          :limite   1.000M,
                          :cliente  "000.111.222-33"}]
       (is (= (s/validate CartaoSchema cartao-valido)
-             cartao-valido)))))
+             cartao-valido))))
+
+  (testing "se o esquema aceita um cartão com número negativo"
+    (let [cartao-invalido {:numero   -1234123412341234,
+                           :cvv      111,
+                           :validade "2023-01",
+                           :limite   1.000M,
+                           :cliente  "000.111.222-33"}]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (s/validate CartaoSchema cartao-invalido)))))
+
+  (testing "se o esquema aceita um cartão com cvv maior que 999"
+    (let [cartao-invalido {:numero   1234123412341234,
+                           :cvv      1000,
+                           :validade "2023-01",
+                           :limite   1.000M,
+                           :cliente  "000.111.222-33"}]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (s/validate CartaoSchema cartao-invalido)))))
+
+  (testing "se o esquema aceita um cartão com data inválida"
+    (let [cartao-invalido {:numero   1234123412341234,
+                           :cvv      999,
+                           :validade "2023/01",
+                           :limite   1.000M,
+                           :cliente  "000.111.222-33"}]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (s/validate CartaoSchema cartao-invalido)))))
+
+  (testing "se o esquema aceita um cartão com limite negativo"
+    (let [cartao-invalido {:numero   1234123412341234,
+                           :cvv      999,
+                           :validade "2023-01",
+                           :limite   -1.000M,
+                           :cliente  "000.111.222-33"}]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (s/validate CartaoSchema cartao-invalido)))))
+
+  (testing "se o esquema aceita um cartão com :cpf inválido"
+    (let [cartao-invalido {:numero   1234123412341234,
+                           :cvv      999,
+                           :validade "2023-01",
+                           :limite   1.000M,
+                           :cliente  "aaa.111.222-33"}]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (s/validate CartaoSchema cartao-invalido))))))
 
 
 (deftest CompraSchema-test
@@ -38,4 +102,49 @@
                          :categoria "Alimentação",
                          :cartao 1234123412341234}]
       (is (= (s/validate CompraSchema compra-valida)
-             compra-valida)))))
+             compra-valida))))
+
+  (testing "se o esquema aceita uma compra com data inválida"
+    (let [compra-invalida {:data "2e22-01-01",
+                         :valor 129.90M,
+                         :estabelecimento "Outback",
+                         :categoria "Alimentação",
+                         :cartao 1234123412341234}]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (s/validate CompraSchema compra-invalida)))))
+
+  (testing "se o esquema aceita uma compra com valor negativo"
+    (let [compra-invalida {:data "2022-01-01",
+                           :valor -129.90M,
+                           :estabelecimento "Outback",
+                           :categoria "Alimentação",
+                           :cartao 1234123412341234}]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (s/validate CompraSchema compra-invalida)))))
+
+  (testing "se o esquema aceita uma compra sem estabelecimento"
+    (let [compra-invalida {:data "2022-01-01",
+                           :valor 129.90M,
+                           :estabelecimento "",
+                           :categoria "Alimentação",
+                           :cartao 1234123412341234}]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (s/validate CompraSchema compra-invalida)))))
+
+  (testing "se o esquema aceita uma compra com categoria inválida"
+    (let [compra-invalida {:data "2022-01-01",
+                           :valor 129.90M,
+                           :estabelecimento "Outback",
+                           :categoria "Alimentacão",
+                           :cartao 1234123412341234}]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (s/validate CompraSchema compra-invalida)))))
+
+  (testing "se o esquema aceita uma compra com número inválido"
+    (let [compra-invalida {:data "2022-01-01",
+                           :valor 129.90M,
+                           :estabelecimento "Outback",
+                           :categoria "Alimentação",
+                           :cartao 124123412346746461234}]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (s/validate CompraSchema compra-invalida))))))
