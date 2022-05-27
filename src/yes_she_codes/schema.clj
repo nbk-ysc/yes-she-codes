b17fd4366613fed49d7db5b32fdd1d91dc9f4781
mariana.moreira <maribrignol.98@gmail.com>
2022-05-27 11:48:38 -0300

    Finishing this week challenge

--- src/yes_she_codes/schema.clj
+++ src/yes_she_codes/schema.clj
@@ -8,26 +8,26 @@
 (s/set-fn-validation! true)
 
 ;DEFINICOES VALIDACAO
-(def Maior-2-caracteres (s/pred util/maior-2-caracteres?))
-(def Valida-cpf (s/pred util/valida-cpf?))
-(def Valida-email (s/pred util/valida-email?))
+(def Maior-2-caracteres (s/constrained s/Str util/maior-2-caracteres?))
+(def Valida-cpf (s/constrained s/Str util/valida-cpf?))
+(def Valida-email (s/constrained s/Str util/valida-email?))
 (def Valida-data-validade (s/pred util/valida-data-validade?))
 (def Valida-data-compra (s/pred util/valida-data-compra?))
 (def Numero-inteiro-entre-0-999 (s/constrained s/Int util/numero-entre-0-999?))
 (def BigDecimal-maior-igual-0 (s/pred util/decimal-maior-igual-0?))
 (def Positivo-e-Inteiro (s/pred pos-int?))
-(def Valida-categorias (s/pred util/valida-categorias?))
+(def Valida-categorias (s/constrained s/Str util/valida-categorias?))
 
 
 ;DEFINIR SCHEMA PARA CLIENTE
-(def ClienteSchema
+(s/def ClienteSchema
   {:nome  Maior-2-caracteres
    :cpf Valida-cpf
    :email Valida-email
    })
 
 ;DEFINIR SCHEMA PARA CARTAO
-(def CartaoSchema
+(s/def CartaoSchema
   {:numero  Positivo-e-Inteiro
    :cvv Numero-inteiro-entre-0-999
    :validade  Valida-data-validade
@@ -36,7 +36,7 @@
    })
 
 ;DEFINIR SCHEMA PARA COMPRA
-(def CompraSchema
+(s/def CompraSchema
   {:data-da-compra Valida-data-compra
    :valor BigDecimal-maior-igual-0
    :estabelecimento  Maior-2-caracteres
@@ -70,5 +70,23 @@
    :email email})
 
 (println (novo-cliente "mari" "034.771.240-14" "mari@gmail.com"))
+(println (novo-cliente "mari" "111.222.333-44" "mari@gmail.com"))
 
 
+(s/defn novo-cartao :- CartaoSchema
+  [numero :- Positivo-e-Inteiro
+   cvv :- Numero-inteiro-entre-0-999
+   validade :- Valida-data-validade
+   limite :- BigDecimal-maior-igual-0
+   cliente :- Valida-cpf]
+  {:numero  numero
+   :cvv cvv
+   :validade  validade
+   :limite limite
+   :cliente cliente})
+
+(println (novo-cartao 4321432143214321
+                      222
+                      "2024-02"
+                      2000M
+                      "333.444.555-66"))
--- test/yes_she_codes/logic_test.clj
+++ test/yes_she_codes/logic_test.clj
@@ -1,31 +1,42 @@
 (ns yes-she-codes.logic-test
   (:require [clojure.test :refer :all]
             [yes-she-codes.schema :refer :all]
-            [schema.core :as s]))
+            [schema.core :as s])
+  (:import (clojure.lang ExceptionInfo)))
 
 (s/set-fn-validation! true)
 
 
 (deftest validate-cliente-test
   (testing "Teste para Adicionar adicionar um Cliente válido"
-    (is (s/validate
-          ClienteSchema {:nome "Viúva Negra"
-                                   :cpf  "333.444.555-66"
-                                   :email "viuva.casca.grossa@vingadoras.com.br"})))
+    (is (novo-cliente "Viúva Negra"
+                      "333.444.555-66"
+                      "viuva.casca.grossa@vingadoras.com.br")))
 
   (testing "Teste para Adicionar adicionar um Cliente não válido"
-    (is (thrown? clojure.lang.ExceptionInfo
-                 "Excessão caso cliente não seja válido"
-                 (s/validate ClienteSchema {:nome "Viúva Negra"
-                                            :cpf  "33344455566"
-                                            :email "v"}))))
+    (is (thrown? ExceptionInfo
+                 (novo-cliente "Viúva Negra"
+                               ""
+                               "viuva.casca.grossa@vingadoras.com.br"))))
 
   (testing "Teste para Adicionar adicionar um Cliente com nome nill"
-    (is (thrown? clojure.lang.ExceptionInfo
-                 "Excessão caso Cliente com nome nill"
-                 (s/validate ClienteSchema {:nome nil
-                                   :cpf  "333.444.555-66"
-                                   :email "viuva.casca.grossa@vingadoras.com.br"})))))
+    (is (thrown? ExceptionInfo
+                 (novo-cliente nil
+                               "333.444.555-66"
+                               "viuva.casca.grossa@vingadoras.com.br"))))
+
+  (testing "Teste para Adicionar adicionar um Cliente com cpf inválido"
+    (is (thrown? ExceptionInfo
+                 (novo-cliente "Viúva Negra"
+                               "66"
+                               "viuva.casca.grossa@vingadoras.com.br"))))
+
+  (testing "Teste para Adicionar adicionar um Cliente com e-mail invalido"
+    (is (thrown? ExceptionInfo
+                 "Excessão caso Cliente com e-mail invalido"
+                 (novo-cliente "Viúva Negra"
+                               "333.444.555-66"
+                               "viuva")))))
 
 (deftest validate-cartao-test
   (testing "Teste para Adicionar adicionar um Cartão válido"
@@ -33,7 +44,48 @@
                                   :cvv 222
                                   :validade "2024-02"
                                   :limite 2000M
-                                  :cliente "333.444.555-66"}))))
+                                  :cliente "333.444.555-66"})))
+
+  (testing "Teste para Adicionar adicionar um Cartão com número negativo"
+    (is (thrown? ExceptionInfo
+                 (s/validate CartaoSchema {:numero -4321432143214321
+                                           :cvv      222
+                                           :validade "2024-02"
+                                           :limite   2000M
+                                           :cliente  "333.444.555-66"}))))
+
+  (testing "Teste para Adicionar adicionar um Cartão com CVV maior que 999"
+    (is (thrown? ExceptionInfo
+                 (s/validate CartaoSchema {:numero 4321432143214321
+                                           :cvv      1000
+                                           :validade "2024-02"
+                                           :limite   2000M
+                                           :cliente  "333.444.555-66"}))))
+
+  (testing "Teste para Adicionar adicionar um Cartão com validade invalida"
+    (is (thrown? ExceptionInfo
+                 (s/validate CartaoSchema {:numero 4321432143214321
+                                           :cvv      222
+                                           :validade "aaaaaa"
+                                           :limite   2000M
+                                           :cliente  "333.444.555-66"}))))
+
+  (testing "Teste para Adicionar adicionar um Cartão com limite negativo"
+    (is (thrown? ExceptionInfo
+                 (s/validate CartaoSchema {:numero 4321432143214321
+                                           :cvv      222
+                                           :validade "2024-02"
+                                           :limite   -2000M
+                                           :cliente  "333.444.555-66"}))))
+
+  (testing "Teste para Adicionar adicionar um Cartão com cliente com cpf invalido"
+    (is (thrown? ExceptionInfo
+                 (s/validate CartaoSchema {:numero 4321432143214321
+                                           :cvv      222
+                                           :validade "2024-02"
+                                           :limite   2000M
+                                           :cliente  "33344455566"}))))
+  )
 
 (deftest validate-compra-test
   (testing "Teste para Adicionar adicionar uma Compra válida"
@@ -41,4 +93,46 @@
                                   :valor 100M
                                   :estabelecimento "Amazon"
                                   :categoria "Casa"
+                                  :cartao 4321432143214321})))
+
+  (testing "Teste para Adicionar adicionar uma Compra com data inválida"
+    (is (thrown? ExceptionInfo
+                 (s/validate CompraSchema {:data-da-compra "aa"
+                                  :valor 100M
+                                  :estabelecimento "Amazon"
+                                  :categoria "Casa"
                                   :cartao 4321432143214321}))))
+
+  (testing "Teste para Adicionar adicionar uma Compra com valor negativo"
+    (is (thrown? ExceptionInfo
+                 (s/validate CompraSchema {:data-da-compra "2022-05-09"
+                                           :valor -100M
+                                           :estabelecimento "Amazon"
+                                           :categoria "Casa"
+                                           :cartao 4321432143214321}))))
+
+  (testing "Teste para Adicionar adicionar uma Compra com estabelecimento vazio"
+    (is (thrown? ExceptionInfo
+                 (s/validate CompraSchema {:data-da-compra "2022-05-09"
+                                           :valor 100M
+                                           :estabelecimento ""
+                                           :categoria "Casa"
+                                           :cartao 4321432143214321}))))
+
+  (testing "Teste para Adicionar adicionar uma Compra com categoria invalida"
+    (is (thrown? ExceptionInfo
+                 (s/validate CompraSchema {:data-da-compra "2022-05-09"
+                                           :valor 100M
+                                           :estabelecimento "Amazon"
+                                           :categoria "Vestuário"
+                                           :cartao 4321432143214321}))))
+
+  (testing "Teste para Adicionar adicionar uma Compra com cartao com numero invalido"
+    (is (thrown? ExceptionInfo
+                 (s/validate CompraSchema {:data-da-compra "2022-05-09"
+                                           :valor 100M
+                                           :estabelecimento "Amazon"
+                                           :categoria "Casa"
+                                           :cartao -1}))))
+
+  )
