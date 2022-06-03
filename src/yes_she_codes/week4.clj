0f391a0f0cbd1232e440ad7697ca6f45b03c407d
Maria Clara Crespo <70768456+mariaclaracrespo@users.noreply.github.com>
2022-06-03 17:07:38 -0300

    Cards obrigatórios - semana 3

--- src/yes_she_codes/week4.clj
+++ src/yes_she_codes/week4.clj
@@ -3,14 +3,13 @@
   (:require [datomic.api :as d]
             [clojure.string :as str]))
 
-;; para iniciar: bin/transactor -Ddatomic.printConnectionInfo=true config/dev-transactor-template.properties
 (def db-uri "datomic:dev://localhost:4334/she-codes")
 
 (defn cria-conexao []
   (d/create-database db-uri)
   (d/connect db-uri))
 
-(defn apaga-banco []
+(defn apaga-banco! []
   (pprint (d/delete-database db-uri)))
 
 
@@ -59,21 +58,44 @@
 
 
 (defn carrega-compras-no-banco!
-  ([]
-   (carrega-compras-no-banco! (abre-csv "resources/compras.csv")))
+  ([conexao]
+   (carrega-compras-no-banco! conexao (abre-csv "resources/compras.csv")))
 
-  ([lista-de-listas]
-    (map (comp (partial salva-compra! (cria-conexao)) nova-compra) lista-de-listas)))
+  ([conexao lista-de-listas]
+    (map (comp (partial salva-compra! conexao) nova-compra) lista-de-listas)))
 
 (defn lista-compras!
   [conexao]
   (let [db (d/db conexao)]
-    (vec (d/q '[:find ?entidade
-                :where [?entidade :compra/data]] db))))
+    (vec (flatten (d/q '[:find (pull ?entidade [:compra/data
+                                                :compra/valor
+                                                :compra/estabelecimento
+                                                :compra/categoria
+                                                :compra/cartao])
+                         :where [?entidade :compra/data]] db)))))
 
-;
-;(defn lista-compras-por-cartao!
+
+(defn lista-compras-por-cartao!
+  [conexao cartao]
+  (let [db (d/db conexao)]
+    (vec (flatten (d/q '[:find (pull ?entidade [*])
+                         :in $ ?cartao-buscado
+                         :where [?entidade :compra/cartao ?cartao-buscado]] db cartao)))))
+
+
+(defn lista-compras-por-cartao-e-mes!
+  [conexao cartao mes]
+  (let [db (d/db conexao)]
+    (vec (flatten (d/q '[:find (pull ?entidade [*])         ;(pull ?entidade [*])
+                         :in $ ?cartao-buscado ?mes-buscado
+                         :where [?entidade :compra/cartao ?cartao-buscado]
+                         [?entidade :compra/data ?data-buscada]
+                         [(re-find #"\d{4}-(\d{2})-\d{2}" ?data-buscada) ?matcher]
+                         [(second ?matcher) ?mes]
+                         [(= ?mes ?mes-buscado)]
+                         ] db cartao mes)))))
+
+
+;(defn ista-gastos-por-categoria!
 ;  [conexao cartao]
-;  (let [db (d/db conexao)]
-;  (vec (d/q '[:find ?entidade
-;              :where [?entidade :compra/cartao cartao]] db))))
+;  )
--- src/yes_she_codes/week4_testes.clj
+++ src/yes_she_codes/week4_testes.clj
@@ -1,21 +1,30 @@
 (ns yes-she-codes.week4-testes
-  (:require [yes-she-codes.week4 :refer [apaga-banco cria-conexao
+  (:require [datomic.api :as d]
+            [yes-she-codes.week4 :refer [apaga-banco! cria-conexao
                                          schema-datomic nova-compra salva-compra!
                                          carrega-compras-no-banco!
-                                         lista-compras! lista-compras-por-cartao!]]))
+                                         lista-compras! lista-compras-por-cartao!
+                                         lista-compras-por-cartao-e-mes!]]))
 
-(apaga-banco)
+;; para iniciar: bin/transactor -Ddatomic.printConnectionInfo=true config/dev-transactor-template.properties
+;; console: bin/console -p 8080 dev datomic:dev://localhost:4343/
+
+
+(apaga-banco!)
 (def conn (cria-conexao))
 (schema-datomic conn)
+(println (d/db conn))  ;; :db-after
 
 (def exemplo-compra (nova-compra "2022-01-01" 129.90M, "Outback",
                                  "Alimentação", 1234123412341234))
-(println exemplo-compra)
+;;(println exemplo-compra)
 
-(salva-compra! conn {:compra/data "data gwqkgd"})
+(salva-compra! conn exemplo-compra)
 
-(carrega-compras-no-banco!)
+(carrega-compras-no-banco! conn)  ;; csv
 
 (lista-compras! conn)
 
-;(lista-compras-por-cartao! conn 3939393939393939)
+(lista-compras-por-cartao! conn 3939393939393939)
+
+(lista-compras-por-cartao-e-mes! conn 3939393939393939 "04")
