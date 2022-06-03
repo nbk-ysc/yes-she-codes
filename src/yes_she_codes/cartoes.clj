fb16ca3e7b5bc60c1e0a39633656be432759d91c
Beatriz Cerqueira <beatriz_cbj@hotmail.com>
2022-06-03 11:44:12 -0300

    feat: adiciona schemas de clientes, cartoes e compras

--- src/yes_she_codes/cartoes.clj
+++ src/yes_she_codes/cartoes.clj
@@ -1,18 +1,60 @@
 (ns yes_she_codes.cartoes
-  (:require [yes_she_codes.db :as y.db]))
+  (:require [yes_she_codes.db :as y.db]
+            [schema.core :as s]))
 
-(defn novo-cartao [numero cvv validade limite cliente]
+(s/set-fn-validation! true)
+
+(defn numero-do-cartao-valido? [numero-do-cartao]
+  (and (> numero-do-cartao 0) (< numero-do-cartao 10000000000000000)))
+
+(defn cvv-valido? [cvv]
+  (and (> cvv 0) (< cvv 1000)))
+
+(defn validade-valida? [validade]
+  (= (re-find #"^[0-9]{4}\-(?:[0][1-9]|[1][0-2])$" validade)
+     validade))
+
+(defn limite-valido? [limite]
+  (>= limite 0))
+
+(defn cliente-valido? [cpf]
+  (= (re-find #"^\d{3}\.\d{3}\.\d{3}\-\d{2}$" cpf)
+     cpf))
+
+(def NumeroDoCartaoSchema (s/constrained s/Int numero-do-cartao-valido?))
+(def CVVSchema (s/constrained s/Int cvv-valido?))
+(def ValidadeSchema (s/constrained s/Str validade-valida?))
+(def LimiteSchema (s/constrained BigDecimal limite-valido?))
+(def ClienteSchema (s/constrained s/Str cliente-valido?))
+
+(def CartaoSchema
+  {:numero   NumeroDoCartaoSchema,
+   :cvv      CVVSchema,
+   :validade ValidadeSchema,
+   :limite   LimiteSchema,
+   :cliente  ClienteSchema})
+
+(def CartoesSchema [CartaoSchema])
+
+(s/defn novo-cartao :- CartaoSchema
+  [numero :- NumeroDoCartaoSchema,
+   cvv :- CVVSchema,
+   validade :- ValidadeSchema,
+   limite :- LimiteSchema,
+   cliente :- ClienteSchema]
   {:numero   numero,
    :cvv      cvv,
    :validade validade,
    :limite   limite,
    :cliente  cliente})
 
-(defn transforma-cartao [[numero cvv validade limite cliente]]
+; To do: Colocar schema no parâmetro
+(s/defn transforma-cartao :- CartaoSchema [[numero cvv validade limite cliente]]
   (novo-cartao numero cvv validade limite cliente))
 
-(defn transforma-cartoes [cartoes]
+; To do: Colocar schema no parâmetro
+(s/defn transforma-cartoes :- CartoesSchema [cartoes]
   (map transforma-cartao cartoes))
 
-(defn lista-cartoes []
+(s/defn lista-cartoes :- CartoesSchema []
   (transforma-cartoes (y.db/registros-de-cartoes)))
--- src/yes_she_codes/compras.clj
+++ src/yes_she_codes/compras.clj
@@ -1,20 +1,66 @@
 (ns yes_she_codes.compras
-  (:require [yes_she_codes.db :as y.db]))
+  (:require [yes_she_codes.db :as y.db]
+            [schema.core :as s]))
 
-(defn nova-compra [data valor estabelecimento categoria cartao]
+(defn id-valido? [id]
+  (>= id 0))
+
+(defn data-valida? [data]
+  (= (re-find #"^[0-9]{4}\-(?:[0][1-9]|[1][0-2])\-(?:0[1-9]|[12][0-9]|3[01])$" data)
+     data))
+
+(defn valor-no-cartao-valido? [valor-no-cartao]
+  (>= valor-no-cartao 0))
+
+(defn estabelecimento-valido? [estabelecimento]
+  (>= (count estabelecimento) 2))
+
+(defn categoria-valida? [categoria]
+  (let [categorias ["Alimentação", "Automóvel", "Casa", "Educação", "Lazer", "Saúde"]]
+    (not (nil? (some #{categoria} categorias)))))
+
+(defn numero-do-cartao-valido? [numero-do-cartao]
+  (and (> numero-do-cartao 0) (< numero-do-cartao 10000000000000000)))
+
+(def Nil (s/pred nil?))
+(def IdSchema (s/maybe (s/constrained s/Int id-valido?)))
+(def DataSchema (s/constrained s/Str data-valida?))
+(def ValorNoCartaoSchema (s/constrained BigDecimal valor-no-cartao-valido?))
+(def EstabelecimentoSchema (s/constrained s/Str estabelecimento-valido?))
+(def CategoriaSchema (s/constrained s/Str categoria-valida?))
+(def CartaoSchema (s/constrained s/Int numero-do-cartao-valido?))
+
+(def CompraSchema
+  {(s/optional-key :id) IdSchema,
+   :data                DataSchema,
+   :valor               ValorNoCartaoSchema,
+   :estabelecimento     EstabelecimentoSchema,
+   :categoria           CategoriaSchema,
+   :cartao              CartaoSchema})
+
+(def ComprasSchema [CompraSchema])
+
+(s/defn nova-compra :- CompraSchema
+  [data :- DataSchema,
+   valor :- ValorNoCartaoSchema,
+   estabelecimento :- EstabelecimentoSchema,
+   categoria :- CategoriaSchema,
+   cartao :- CartaoSchema]
   {:data            data,
    :valor           valor,
    :estabelecimento estabelecimento,
    :categoria       categoria,
    :cartao          cartao})
 
-(defn transforma-compra [[data valor estabelecimento categoria cartao]]
+; To do: Colocar schema no parâmetro
+(s/defn transforma-compra :- CompraSchema [[data valor estabelecimento categoria cartao]]
   (nova-compra data valor estabelecimento categoria cartao))
 
-(defn transforma-compras [compras]
+; To do: Colocar schema no parâmetro
+(s/defn transforma-compras :- ComprasSchema [compras]
   (map transforma-compra compras))
 
-(defn lista-compras []
+(s/defn lista-compras :- ComprasSchema []
   (transforma-compras (y.db/registros-de-compras)))
 
 ; ======================================================
@@ -23,58 +69,68 @@
 
 (def repositorio-de-compras (atom []))
 
-(defrecord Compra [id, data, valor, estabelecimento, categoria, cartao])
+(s/defrecord Compra [id :- IdSchema,
+                     data :- DataSchema,
+                     valor :- ValorNoCartaoSchema,
+                     estabelecimento :- EstabelecimentoSchema,
+                     categoria :- CategoriaSchema,
+                     cartao :- CartaoSchema])
 
 ; ======================================================
 
-(defn compra-com-id [compras-cadastradas compra-sem-id]
+(s/defn compra-com-id :- CompraSchema [compras-cadastradas :- ComprasSchema, compra-sem-id :- CompraSchema]
   (assoc compra-sem-id :id (count compras-cadastradas)))
 
-(defn insere-compra [compras-cadastradas compra-sem-id]
+(s/defn insere-compra :- ComprasSchema [compras-cadastradas :- ComprasSchema, compra-sem-id :- CompraSchema]
   (conj compras-cadastradas (compra-com-id compras-cadastradas compra-sem-id)))
 
-(defn insere-compra! [compra]
-  (swap! repositorio-de-compras insere-compra compra))
+(s/defn insere-compra! :- Nil [compra :- CompraSchema]
+  (swap! repositorio-de-compras insere-compra compra)
+  nil)
 
 ; ======================================================
 
-(defn reset-compras! []
-  (reset! repositorio-de-compras []))
+(s/defn reset-compras! :- Nil []
+  (reset! repositorio-de-compras [])
+  nil)
 
-(defn transforma-compra! [[data valor estabelecimento categoria cartao]]
+; To do: Colocar schema no parâmetro
+(s/defn transforma-compra! :- Nil [[data valor estabelecimento categoria cartao]]
   (insere-compra! (map->Compra {:data            data,
                                 :valor           valor,
                                 :estabelecimento estabelecimento,
                                 :categoria       categoria,
-                                :cartao          cartao})))
+                                :cartao          cartao}))
+  nil)
 
-(defn carrega-compras! []
+(s/defn carrega-compras! :- Nil []
   (reset-compras!)
   (doseq
     [compra (y.db/registros-de-compras)]
-    (transforma-compra! compra)))
+    (transforma-compra! compra))
+  nil)
 
 ; ======================================================
 
-(defn lista-compras! []
+(s/defn lista-compras! :- ComprasSchema []
   @repositorio-de-compras)
 
 ; ======================================================
 
-(defn compara-ids [id-buscado compra]
+(s/defn compara-ids :- (s/maybe CompraSchema) [id-buscado :- IdSchema, compra :- CompraSchema]
   (if (= id-buscado (:id compra))
     compra))
 
-(defn pesquisa-compra-por-id [compras-cadastradas id-buscado]
+(s/defn pesquisa-compra-por-id :- (s/maybe CompraSchema) [compras-cadastradas :- ComprasSchema, id-buscado :- IdSchema]
   (some #(compara-ids id-buscado %) compras-cadastradas))
 
-(defn pesquisa-compra-por-id! [id-buscado]
+(s/defn pesquisa-compra-por-id! :- (s/maybe CompraSchema) [id-buscado :- IdSchema]
   (pesquisa-compra-por-id @repositorio-de-compras id-buscado))
 
 ; ======================================================
 
-(defn exclui-compra [compras-cadastradas id-para-excluir]
+(s/defn exclui-compra :- ComprasSchema [compras-cadastradas :- ComprasSchema, id-para-excluir :- IdSchema]
   (remove #(compara-ids id-para-excluir %) compras-cadastradas))
 
-(defn exclui-compra! [id-para-excluir]
+(s/defn exclui-compra! :- ComprasSchema [id-para-excluir :- IdSchema]
   (swap! repositorio-de-compras exclui-compra id-para-excluir))
