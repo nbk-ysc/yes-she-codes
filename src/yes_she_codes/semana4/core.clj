0fa3251941cb2f6c479566e5de40e5012d1fdc92
Gardeli <30728184+Gardeli@users.noreply.github.com>
2022-06-03 17:29:20 -0300

    salva-compra criado
    Iniciado tb a listagem das compras via query

--- src/yes_she_codes/semana4/core.clj
+++ src/yes_she_codes/semana4/core.clj
@@ -1,8 +1,8 @@
 (ns yes_she_codes.semana4.core
-  (:use clojure.pprint)
   (:require [datomic.api :as d]
-            [yes_she_codes.semana4.db :as db]
-            [yes-she-codes.semana4.compra :as compra]))
+            [yes-she-codes.semana4.compra :as compra]
+            [yes_she_codes.semana4.db :as db])
+  (:use [clojure.pprint]))
 
 
 ;Chama funçao que cria o banco e faz conexão
@@ -11,7 +11,17 @@
 
 (db/cria-esquema conn)
 
-(let [compra (compra/nova-compra "/compra_outback" "2022-01-02" 345.83M "Outback" "Alimentacao" 1234123412341234)]
-  (d/transact conn [compra]))
+;Funcao salva dados de novas compras no banco
+(defn salva-compra! [conn compra]
+  (let [compra compra]
+    (d/transact conn [compra])))
 
-(d/q )
+(salva-compra! conn (compra/nova-compra "/compra_dentista" "2022-01-02" 260.00M "Dentista" "Saúde" 1234123412341234))
+
+(defn lista-compras! [conn]
+  (let [conn conn]
+    (def db (d/db conn))                                    ;Snapshot do banco nesse exato momento
+    (d/q '[:find ?entidade
+           :where [?entidade :compra/data]] db)))           ;Select que tras todas as compras de um estabelecimento
+
+(pprint (lista-compras! conn))
