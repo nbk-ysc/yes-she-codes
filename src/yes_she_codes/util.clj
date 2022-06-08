1b914af449c1f6189b457c3da050d8db35964d4f
Cácio Costa <cacio.cjcs@gmail.com>
2022-06-08 20:38:50 -0300

    finalizar semana 4 com datomic

--- src/yes_she_codes/util.clj
+++ src/yes_she_codes/util.clj
@@ -6,21 +6,6 @@
   (Long/parseLong (clojure.string/replace valor #" " "")))
 
 
-(defprotocol ExtratorDeMes
-  (mes-da-data [data]
-    "Dada uma data, extrai o valor do mês como um inteiro de 1 a 12"))
-
-(extend-type java.lang.String ExtratorDeMes
-  (mes-da-data [data]
-    (->> data
-         (re-matches #"\d{4}-(\d{2})-\d{2}")
-         second
-         Long/valueOf)))
-
-(extend-type java.time.LocalDate ExtratorDeMes
-  (mes-da-data [data]
-    (.getValue (time/month data))))
-
 (defn data-valida? [data]
   (re-matches #"\d{4}-(\d{2})-\d{2}" data))
 
@@ -46,5 +31,58 @@
 (def ValorPositivo (s/constrained BigDecimal (comp not neg?)))
 
 
+(defn local-date-time->date [valor]
+  (if (time/local-date-time? valor)
+    (->> (.atZone valor (time/zone-id))
+         .toInstant
+         java.util.Date/from)
+    valor))
 
 
+(defn local-date->date [valor]
+  (if (time/local-date? valor)
+    (->> (time/zone-id)
+         (.atStartOfDay valor)
+         (.toLocalDateTime)
+         (local-date-time->date))
+    valor))
+
+
+(defn date->local-date-time [valor]
+  (if (= java.util.Date (class valor))
+    (-> (.toInstant valor)
+        (java.time.LocalDateTime/ofInstant (time/zone-id)))
+    valor))
+
+
+(defn- converte-valores-de-um-mapa [fn mapa]
+  (clojure.walk/postwalk fn mapa))
+
+(def converte-java-time-para-date
+  (partial converte-valores-de-um-mapa (comp local-date->date local-date-time->date)))
+
+(def converte-date-para-java-time
+  (partial converte-valores-de-um-mapa date->local-date-time))
+
+
+(defprotocol ExtratorDeMes
+  (mes-da-data [data]
+    "Dada uma data, extrai o valor do mês como um inteiro de 1 a 12"))
+
+(extend-type java.lang.String ExtratorDeMes
+  (mes-da-data [data]
+    (->> data
+         (re-matches #"\d{4}-(\d{2})-\d{2}")
+         second
+         Long/valueOf)))
+
+(extend-type java.time.LocalDate ExtratorDeMes
+  (mes-da-data [data]
+    (.getValue (time/month data))))
+
+(extend-type java.util.Date ExtratorDeMes
+  (mes-da-data [data]
+    (-> data
+        date->local-date-time
+        .toLocalDate
+        mes-da-data)))
