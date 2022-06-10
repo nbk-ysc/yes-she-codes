66d3630ad6841c4110d8cfeec728e1d119a471ad
DaianedaSilva <daiane.supereira@gmail.com>
2022-06-10 11:48:58 -0300

    semana 4 fim

--- src/yes_she_codes/semana1/cartao.clj
+++ src/yes_she_codes/semana1/cartao.clj
@@ -1,18 +1,19 @@
 (ns yes-she-codes.semana1.cartao
-  (:require yes-she-codes.semana1.compra :as yes-she-codes.semana1.compras ))
+  (:require [yes-she-codes.semana1.db :as y.db]
+            [yes-she-codes.semana1.compras :as y.compras]
+            [yes-she-codes.semana1.utilities  :as y.logica]))
 
-;(:require [yes-she-codes.semana1.db :as y.db]
-;            [yes-she-codes.semana1.compras :as y.compras]
-;            [yes-she-codes.utilities.logica  :as y.utilities])
+
 (def lista-compra [y.db/compra3, y.db/compra3, y.db/compra3 ] )
 
 (defn total-gasto
   [ compras ]
   (->> compras
-       (map y.utilities/get-valor)
+       (map y.logica/get-valor)
        (reduce +)))
 
 (total-gasto lista-compra)
+
 
 (defn total-gasto-no-mes
   [mes lista-compras]
